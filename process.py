57456b6cf1c3e4bd74d7a3746c931d965fa64935
Filip Ćosović <46849858+redemption99@users.noreply.github.com>
2019-12-30 02:38:35 +0100

    Update process.py

--- process.py
+++ process.py
@@ -2,34 +2,34 @@ import numpy as np
 import cv2
 
 def process_image(image_data):
-    # dimenzije slike
+    # image dimensions
     width, height = image_data.shape
 
-    # kreiramo novu kvadratnu sliku, popunjenu jedinicama (bela boja), koja predstavlja pocetnu sliku prosirenu neutralnim pikselima do kvadrata
+    # creating new squared image, filled with ones (white color), which represent the initial image exceeded by neutral pixels to the square
     padded_image = np.ones((max(width, height), max(width, height)))
 
-    # nadjemo gornji levi cosak centrirane slike unutar prosirene slike
+    # finding upper left corner of centered image inside the padded image
     center_x = (max(width, height) - width) // 2
     center_y = (max(width, height) - height) // 2
 
-    # prepisemo pocetnu sliku u centralni deo nove slike
+    # writing the initial image inside center part of padded iamge
     padded_image[center_x: center_x + width, center_y: center_y + height] = image_data / 255
 
-    # smanjimo sliku na velicinu od 200x200 piksela
+    # resizing image to 229x229 pixels
     scaled_image = cv2.resize(padded_image, (229, 229))
 
-    # svaki piksel postaje srednja vrednost njega i okolnih 8 piksela kako bi se eliminisao sum
+    # average pooling
     kernel = np.ones((3, 3), np.float32) / 9
     averaged_image = cv2.filter2D(scaled_image, -1, kernel)
 
-    # svaki piksel postaje 0 ili 1, za granicu uzimamo 0.95 posto je to srednja vrednost piksela
+    # setting leaf pixels to 1, non leaf to 0. ~0.95 is the average value of pixels
     rounded_image = (averaged_image < 0.95).astype(int) * 1.0
 
-    # primenjujemo Laplasov filter za detektovanje svih ivica
+    # Laplacian filter for contour detection
     edges_image = cv2.Laplacian(rounded_image, cv2.CV_64F)
 
-    # opet zaokruzujemo sliku na nule i jedinice
+    # setting pixels on the contour of leaf to 1, other pixels to 0
     edges_image = (edges_image > 0.5).astype(int) * 1.0
 
-    # vracamo obradjenu sliku
+    # returning processed image
     return edges_image
